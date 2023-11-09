dbc0b33ac5c067cdca98a5fdc2506ce1f3f6a321
Alexis Dominguez <alexis2033ag@outlook.com>
2023-11-08 18:18:52 -0600

    Se han agregado los botones de menu hambrguesa y el boton de dark mode, se agrego su funcionalidad

---
+++ src/js/app.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    eventListeners();
+    darkMode();
+    comprobar();
+});
+
+function comprobar(){
+
+    if(localStorage.getItem('darkMode')==='on'){
+        document.body.classList.add("dark-mode");
+    } else{
+        document.body.classList.remove("dark-mode");
+    }
+
+    console.log(localStorage.getItem('darkMode'));
+}
+function darkMode(){
+    const darkModeBtn=document.querySelector('.dark-mode-boton');
+    darkModeBtn.addEventListener('click',function(){
+        document.body.classList.toggle('dark-mode');
+        if(document.body.classList.contains("dark-mode")){
+            localStorage.setItem("darkMode", "on");
+        } else{
+            localStorage.setItem("darkMode", "off");
+        }
+    })
+}
+
+function eventListeners(){
+    const mobileMenu = document.querySelector('.mobile-menu');
+    mobileMenu.addEventListener('click',navegacionResponsive);
+}
+
+function navegacionResponsive(){
+    const navegacion = document.querySelector('.navegacion');
+    navegacion.classList.toggle("mostrar");
+}
