d79105ca73d871d3aaa25df62c23674bac4185d3
Alexis Dominguez <alexis2033ag@outlook.com>
2023-11-10 14:26:03 -0600

    Creacion de los apartados de administrador para el CRUD, hasta ahora pre finalizacion del proceso de crear

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -4,11 +4,15 @@
     $db=conectarDB();
     //var_dump($db);
     $errores=[];
+    $queryVendedores="SELECT * FROM vendedores";
+    $vendedores=mysqli_query($db,$queryVendedores);
+    //var_dump(mysqli_fetch_assoc($vendedores));
+   
 
     if($_SERVER["REQUEST_METHOD"]==="POST"){
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
+        // echo "<pre>";
+        // var_dump($_POST);
+        // echo "</pre>";
 
         $titulo=$_POST["titulo"];
         $precio=$_POST["precio"];
@@ -19,8 +23,8 @@
         $wc=$_POST["wc"];
         $estacionamiento=$_POST["estacionamiento"];
         $vendedoresId=$_POST["vendedor"];
-        $query= " INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento, vendedores_id) VALUES ('$titulo','$precio','$descripcion','$habitaciones','$wc', '$estacionamiento', '$vendedoresId') ";
-        echo $query;
+        $creado = date('Y/m/d');
+       
         
         //Validar campos vacios
         if(!$titulo){
@@ -45,15 +49,23 @@
             $errores[]="Debes seleccionar el vendedor";
         }
 
-        var_dump($errores);
-        exit;
+        
+        // echo "<pre>";
+        // var_dump($errores);
+        // echo "</pre>";
 
+        if(empty($errores)){
+            $query= " INSERT INTO propiedades (titulo,precio,descripcion,habitaciones,wc,estacionamiento, creado, vendedores_id) VALUES ('$titulo','$precio','$descripcion','$habitaciones','$wc', '$estacionamiento','$creado', '$vendedoresId') ";
+            
+       
         $resultado = mysqli_query($db,$query);
 
         if($resultado){
-            echo 'Insertado correctamente';
+            header('Location: /bienes_raices/admin/index.php');
         }
 
+        }
+        
     }
     
 
@@ -63,43 +75,49 @@
 <main class="contenedor seccion">
     <h2>Crear</h2>
     <a class="boton-verde" href="../index.php">Volver</a>
-
+    <?php foreach($errores as $error): ?>
+        <div class="alerta error"> <?php echo $error; ?> </div>
+        <?php endforeach ?>
     <form action="/bienes_raices/admin/propiedades/crear.php" class="formulario" method="POST">
         <fieldset>
             <legend>Informacion General de la propiedad</legend>
 
             <label for="titulo">Titulo:</label>
-            <input type="text" name="titulo" id="titulo" placeholder="Titulo de la propiedad">
+            <input type="text" name="titulo" id="titulo" placeholder="Titulo de la propiedad" value="<?php echo $titulo ?>">
 
             <label for="precio">precio:</label>
-            <input type="text" name="precio" id="precio" placeholder="Precio de la propiedad">
+            <input type="text" name="precio" id="precio" placeholder="Precio de la propiedad" value="<?php echo $precio ?>">
 
             <label for="img">Imagen</label>
             <input type="file" name="imagen" id="imagen" accept="image/jpeg, image/png">
 
             <label for="desc">Descripcion</label>
-            <textarea name="descripcion" id="descripcion" cols="30" rows="10"></textarea>
+            <textarea name="descripcion" id="descripcion" cols="30" rows="10"><?php echo $descripcion ?></textarea>
         </fieldset>
         <fieldset>
             <legend>Informacion de la propiedad</legend>
             <label for="habitaciones">Habitaciones</label>
-            <input type="number" name="habitaciones" id="rooms" max="9" min="1">
+            <input type="number" name="habitaciones" id="rooms" max="9" min="1" value="<?php echo $habitaciones ?>">
             <label for="wc">WC</label>
-            <input type="wc" name="wc" id="rooms" max="9" min="1">
+            <input type="wc" name="wc" id="rooms" max="9" min="1" value="<?php echo $wc ?>">
             <label for="estacionamiento">Estacionamiento</label>
-            <input type="estacionamiento" name="estacionamiento" id="rooms" max="9" min="1">
+            <input type="estacionamiento" name="estacionamiento" id="rooms" max="9" min="1" value="<?php echo $estacionamiento ?>">
         </fieldset>
 
         <fieldset>
             <legend>Vendedor</legend>
             <select name="vendedor" id="vendedor">
                 <option value="" selected disabled>-- Selecciona --</option>
-                <option value="1">Alexis</option>
-                <option value="2">Karen</option>
+                <?php while($row = mysqli_fetch_assoc($vendedores)) { ?>
+                    <option value=" <?php echo $row['id'] ?>"  <?php echo $vendedoresId==$row['id'] ? "selected" : ''?> > <?php echo $row['nombre'].' '.$row['apellido']; ?> </option>
+                <?php }?>
             </select>
+            <?php echo 'Hola' ?>
         </fieldset>
         <input type="submit" value="Crear Propiedad" class="boton-verde">
     </form>
+
+   
 
 </main>
 <?php 
